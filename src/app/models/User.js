8fb5c643eeb773680bc95a1dceb5d34d673357c2
oliveira.bruno <oliveira.bruno@greenbeltti.onmicrosoft.com>
2022-05-07 09:10:40 -0300

    Desenvolvido model de User e o loader das models

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -5,7 +5,7 @@ class User extends Model {
     super.init({
       name: Sequelize.STRING,
       email: Sequelize.STRING,
-      provider_hash: Sequelize.STRING,
+      password_hash: Sequelize.STRING,
       provider: Sequelize.BOOLEAN
     })
   }
